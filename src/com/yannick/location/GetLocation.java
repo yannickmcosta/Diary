ce2d83419548cc1232e974bf3a92c5eb0eec88ab
Yannick McCabe-Costa <yanmc@me.com>
2013-03-21 16:10:15 +0000

    Changes to get location

--- src/com/yannick/location/GetLocation.java
+++ src/com/yannick/location/GetLocation.java
@@ -3,23 +3,34 @@ package com.yannick.location;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
-import android.location.Criteria;
+//import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.widget.TextView;
+//import android.widget.TextView;
 import android.widget.Toast;
-import com.yannick.diary.R;
+//import com.yannick.diary.R;
 
 public class GetLocation extends Activity implements LocationListener {
-  public TextView mlatituteField;
-  public TextView mlongitudeField;
-  public TextView mproviderField;
   private LocationManager locationManager;
   private String provider;
 
+  
+  public double getLatitude() {
+	  locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+	  locationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, this, null);
+	  Location location = locationManager.getLastKnownLocation(provider);
+	  return (double) (location.getLatitude());
+  };
+  
+  public double getLongitude() {
+	  locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+	  locationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, this, null);
+	  Location location = locationManager.getLastKnownLocation(provider);
+	  return (double) (location.getLongitude());  
+  };
   
 /** Called when the activity is first created. */
 
@@ -27,27 +38,27 @@ public class GetLocation extends Activity implements LocationListener {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     //setContentView(R.layout.activity_show_location);
-    mlatituteField = (TextView) findViewById(R.id.locEdt_et_lat);
-    mlongitudeField = (TextView) findViewById(R.id.locEdt_et_lon);
+    //mlatituteField = (TextView) findViewById(R.id.locEdt_et_lat);
+    //mlongitudeField = (TextView) findViewById(R.id.locEdt_et_lon);
     //providerField = (TextView) findViewById(R.id.GPSprov1);
 
     // Get the location manager
-    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+    //locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
     // Define the criteria how to select the location provider -> use
     // default
-    Criteria criteria = new Criteria();
-    provider = locationManager.getBestProvider(criteria, false);
-    Location location = locationManager.getLastKnownLocation(provider);
+    //Criteria criteria = new Criteria();
+    //provider = locationManager.getBestProvider(criteria, false);
+    //Location location = locationManager.getLastKnownLocation(provider);
 
     // Initialize the location fields
-    if (location != null) {
-      System.out.println("Provider " + provider + " has been selected.");
-      onLocationChanged(location);
-    } else {
-      mlatituteField.setText("Location not available");
-      mlongitudeField.setText("Location not available");
-      mproviderField.setText("Provider Not Available");
-    }
+    //if (location != null) {
+    //  System.out.println("Provider " + provider + " has been selected.");
+    //  onLocationChanged(location);
+    //} else {
+    //  mlatituteField.setText("Location not available");
+    // mlongitudeField.setText("Location not available");
+    //  mproviderField.setText("Provider Not Available");
+   // }
   }
 
   /* Request updates at startup */
@@ -68,12 +79,12 @@ public class GetLocation extends Activity implements LocationListener {
 //@Override
   public void onLocationChanged(Location location) {
 	locationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, this, null);
-    double lat = (double) (location.getLatitude());
-    double lng = (double) (location.getLongitude());
-    String prov = (String) (provider);
-    mlatituteField.setText(String.valueOf(lat));
-    mlongitudeField.setText(String.valueOf(lng));
-    mproviderField.setText(String.valueOf(prov));
+    //double lat = (double) (location.getLatitude());
+    //double lng = (double) (location.getLongitude());
+    //String prov = (String) (provider);
+    //mlatituteField.setText(String.valueOf(lat));
+    //mlongitudeField.setText(String.valueOf(lng));
+    //mproviderField.setText(String.valueOf(prov));
   }
 
   //@Override
